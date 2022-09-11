2fde299d8b0bc45a827f82d521909a3c1ecddd76
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-10 21:20:07 -0700

    Home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome Fellow Visitors!</h1>
+            <h2>Robertson Rojas</h2>
+            <h3>Full-Stack Developer</h3>
+            <p>
+                Check out my portfolio!
+            </p>
+        </div>
+    )
+}
