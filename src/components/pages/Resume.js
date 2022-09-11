5a8819f77533745b756dd606a5f65b06f2024a1f
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-10 21:26:23 -0700

    resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>
+                Coding Technologies and skills I've worked on so far:
+                <ul>
+                    <li>Html + CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node/Express</li>
+                    <li>MYSQL + Sequelize</li>
+                    <li>MongoDB</li>
+                    <li>React</li>
+                </ul>
+            </p>
+        </div>
+    )
+}
