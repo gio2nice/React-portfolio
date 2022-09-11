6e8127d9d7efd9dc4aa8bb62839c13d0565f7c03
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-10 21:17:40 -0700

    div change

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,60 +3,65 @@ import React from 'react';
 export default function Contact() {
     return (
         <div>
-            <h1>Contact Me</h1>
-            <div className='ContactForm'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-12 text-center'>
-                            <div className='contactForm'>
-                                <form id='contact-form' noValidate>
-                                    <div className='row formRow'>
-                                        <div className='col-6'>
-                                            <input
-                                                type='text'
-                                                name='name'
-                                                className='form-control formInput'
-                                                placeholder='Name'
-                                            ></input>
-                                        <div />
-                                            <div className='col-6'>
-                                                <input
-                                                    type='email'
-                                                    name='email'
-                                                    className='form-control formInput'
-                                                    placeholder='Email address'
-                                                ></input>
-                                            </div>
-                                    </div>
-                                        <div className='row formRow'>
-                                            <div className='col'>
-                                                <input
-                                                    type='text'
-                                                    name='subject'
-                                                    className='form-control formInput'
-                                                    placeholder='Subject'
-                                                ></input>
-                                            </div>
-                                        </div>
-                                        <div className='row formRow'>
-                                            <div className='col'>
-                                                <textarea
-                                                    row={3}
-                                                    name='message'
-                                                    className='form-control formInput'
-                                                    placeholder='Message'
-                                                ></textarea>
-                                            </div>
-                                        </div>
-                                        <button className='submit-btn' type='submit'>
-                                            Submit
-                                        </button>
-                                </form>
-                            </div>
-                        </div>
+        <h1>Contact Me</h1>
+        <div className='ContactForm'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-12 text-center'>
+                <div className='contactForm'>
+                  <form id='contact-form' noValidate>
+                    {/* Row 1 of form */}
+
+                    <div className='row formRow'>
+                      <div className='col-6'>
+                        <input
+                          type='text'
+                          name='name'
+                          className='form-control formInput'
+                          placeholder='Name'
+                        ></input>
+                      </div>
+                      <div className='col-6'>
+                        <input
+                          type='email'
+                          name='email'
+                          className='form-control formInput'
+                          placeholder='Email address'
+                        ></input>
+                      </div>
                     </div>
+                    {/* Row 2 of form */}
+
+                    <div className='row formRow'>
+                      <div className='col'>
+                        <input
+                          type='text'
+                          name='subject'
+                          className='form-control formInput'
+                          placeholder='Subject'
+                        ></input>
+                      </div>
+                    </div>
+                    {/* Row 3 of form */}
+                    <div className='row formRow'>
+                      <div className='col'>
+                        <textarea
+                          rows={3}
+                          name='message'
+                          className='form-control formInput'
+                          placeholder='Message'
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button className='submit-btn' type='submit'>
+                      Submit
+                    </button>
+                  </form>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     )
 }
