881fe5ff072491eca88914d3c194a3ddf0c8a87b
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-11 20:41:41 -0700

    photo update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div class="p-3 mb-2 bg-dark text-white" >
             <h1>About Me</h1>
-            <img src={headshot} className="headshot" alt='My headshot'></img>
+            <img width={350} src={headshot} className="headshot" alt='My headshot'></img>
             <p>
                 Hey everyone! My name is Robertson and I am a new full stack developer ready to kick start my new developer career this fall/winter of 2022. I have just completed the UW Full stack development bootcamp in the course of 3 months.
                 Some skills I feel very confident in are JavaScript, HTML, MYSQL, Express and Node, currently still getting the hang of React but by the day getting better. 
